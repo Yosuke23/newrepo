b427ed81e4037205b9e16a851ed964f87db77b28
vagrant <vagrant@localhost.localdomain>
2019-11-11 11:11:20 +0900

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+  	@user = User.find(params[:id])
+  	  	# debugger
   end
+
+  def new
+  	@user = User.new
+  	  	# debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save # 保存の成功をここで扱う。
+       flash[:success] = "Welcom to Sample App!"
+       redirect_to @user
+       # redirect_to user_url(@user) # 上記に等価
+       # redirect_to user_url(id: @user.id) # 上記に等価
+       # redirect_to "/users/#{@user.id}" # 上記に等価
+    else
+      render 'new' #errorメッセージのページ
+    end
+  end
+
+  private
+
+	  def user_params 
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20191020065702) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "strings"
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
