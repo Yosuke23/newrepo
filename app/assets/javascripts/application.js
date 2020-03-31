e28030c1d870ad1de2578b39868b4dea84767dab
vagrant <vagrant@localhost.localdomain>
2020-03-31 13:18:11 +0900

    Add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require bootstrap
 //= require turbolinks
 //= require bootstrap.min
-//= require_tree .
