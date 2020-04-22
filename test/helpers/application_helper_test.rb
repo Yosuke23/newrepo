6451e931eee1e245eb78bff362b7bf9fe4c38a70
“yosuke” <yosuke4123@gmail.com>
2020-04-22 13:29:23 +0900

    Add done

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "album of memories.."
-    assert_equal full_title("Help"), "Help | album of memories.."
+    assert_equal full_title,         "Cherish"
+    assert_equal full_title("info"), "info | Cherish"
   end
 end
